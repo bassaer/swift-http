fe253fff3245f8e1bed8d5f843eb4afa1bcf7338
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-10-06 17:58:36 +0900

    add json parse

--- swift-http/ViewController.swift
+++ swift-http/ViewController.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let src = ["AAA", "BBB", "CCC"]
+    //let src = ["AAA", "BBB", "CCC"]
+    let jsonSrc = """
+    {"items": ["AAA", "BBB", "CCC", "DDD"]}
+    """
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return src.count
+        return 4
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-        cell.textLabel!.text = src[indexPath.row]
+        
+        let json = try? JSONSerialization.jsonObject(with: jsonSrc.data(using: .utf8)!, options: [])
+        if let dictionary = json as? [String: Any] {
+            if let items = dictionary["items"] as? [String] {
+                cell.textLabel!.text = items[indexPath.row]
+            }
+        }
+        
         return cell
     }
     
